7f1ea1d818bc157163122a5c41c0f4d3f06587d3
ujjwal0kumar <ujjwalraj221@gmail.com>
2023-10-11 12:20:07 +0530

    updated

---
+++ gh.py
@@ -0,0 +1,7 @@
+message = "you are so beautiful. I love nature"
+print(message)
+reply=input("send ur reply: ")
+if reply== "yes":
+    print("thank you but not intrested")
+else:
+     print("bhag ja bsdk")
